92f095145e9e8380ebf859a45860580f4221181b
Rich Shalom <designwebrich@gmail.com>
2026-07-20 14:06:27 -0500

    style: reduce line height of main hero title

--- src/components/home/CinemaHero.tsx
+++ src/components/home/CinemaHero.tsx
@@ -318,7 +318,7 @@ export function CinemaHero() {
             </div>
 
             {/* Apple style headline */}
-            <h1 className="text-5xl md:text-6xl lg:text-[72px] font-black tracking-tight text-neutral-900 leading-[0.95] font-sans pb-2">
+            <h1 className="text-5xl md:text-6xl lg:text-[72px] font-black tracking-tight text-neutral-900 leading-[0.85] font-sans pb-2">
               {t('hero.title_part1')}{" "}
               <span className="text-[#8da9c4]">
                 {t('hero.title_part2')}
